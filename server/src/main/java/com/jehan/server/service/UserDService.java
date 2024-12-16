5ac79be96c7b77d2396e378008617f76c2e474fc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-16 09:33:06 +0530

    Create UserDService.java

---
+++ server/src/main/java/com/jehan/server/service/UserDService.java
@@ -0,0 +1,17 @@
+package com.jehan.server.service;
+
+import com.jehan.server.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByEmail(username).orElseThrow();
+    }
+}
